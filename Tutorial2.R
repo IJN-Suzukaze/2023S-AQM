55e72dbeba0662f9d35b6887d6a55ad67ed86816
HajarElMesa <138592090+HajarElMesa@users.noreply.github.com>
2023-07-06 13:39:39 +0200

    .

--- Tutorial2.R
+++ Tutorial2.R
@@ -30,3 +30,20 @@ vect_perSit <- as.vector(perSit)
 
 barplot(perSit$rel_act)
 barplot(perSit$rel_act,main = 'activity level per service situation', xlab = 'service situation', ylab = 'activity level')
+
+###A2.3:
+Lin_reg1 <- lm(Salesforce$ACT~Salesforce$OWSF+Salesforce$OWSS+Salesforce$WWSF+Salesforce$WWSS)
+summary(Lin_reg1)
+
+Log_reg1 <- glm(Salesforce$ACT~Salesforce$OWSF+Salesforce$OWSS+Salesforce$WWSF+Salesforce$WWSS, 
+                family = binomial(link = 'logit') )
+summary(Log_reg1)
+#interpretation: e^ß
+
+
+
+###If you dont control = gets sucked up in the error ≠ ok, adding IV = controlling for IV###
+###A2.4:
+Log_reg2 <- glm(Salesforce$ACT ~ Salesforce$OWSF+Salesforce$WWSS+Salesforce$WWSS+Salesforce$SPEC+Salesforce$TRAIN+Salesforce$LEAD+Salesforce$TEXP+Salesforce$TSERV+Salesforce$JOIN+Salesforce$LEAV+Salesforce$SIZE+Salesforce$OPEN+Salesforce$PAY+Salesforce$DUR+Salesforce$ASTAT+Salesforce$FAM+Salesforce$CSERV+Salesforce$CEXP,family = binomial(link = 'logit'))
+summary(Log_reg2)
+
