eefcc52a55c66e725a85c8b3b4d57b0e1c0ace0a
HajarElMesa <138592090+HajarElMesa@users.noreply.github.com>
2023-07-06 11:25:28 +0200

    .

---
+++ A2.1.R
@@ -0,0 +1,29 @@
+#####TUTORIAL 2: LOGISTOCAL REGRESSION#####
+install.packages('readxl')
+library('readxl')
+Salesforce <- read_excel('/Users/el/Documents/SS-VUA/Data/Data2.xlsx')
+
+###A2.1: Descriptives###
+summary(Salesforce)
+
+cor(Salesforce)
+round((cor(Salesforce)),2)
+
+install.packages('tidyverse')
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(tidyverse)
+head(Salesforce)
+
+perTech <- Salesforce %>% group_by(technician) %>% summarise(count = n(),
+act = sum(ACT), rel_act=sum(ACT)/n())
+av_obsperTech <- perTech %>% summarise(avg_obs = mean(obs), avg_act = mean(act), avg_rel_act=mean(act)/mean(obs))
+
+
+###A2.2: 
+perSit <- Salesforce %>% group_by(OWSF,OWSS,WWSF,WWSS) %>% summarise(count = n(), act = sum(ACT), rel_act = sum(ACT/n())) 
+
+
+
+
