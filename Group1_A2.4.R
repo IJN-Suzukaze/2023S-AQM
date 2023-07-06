811f2084cdcb67685baf7afe8b54911cef8fa576
Suzukaze <destroyersuzukaze@outlook.com>
2023-07-06 13:43:46 +0200

    Update Group1_A2.4.R

--- Group1_A2.4.R
+++ Group1_A2.4.R
@@ -13,7 +13,6 @@ sale_data = sale_data %>% select(-ASTAT, -LTIME)
 log_reg = glm(ACT ~ OWSF + OWSS + WWSS, family = binomial(link = 'logit'), data = sale_data )
 summary(log_reg)
 
-log_reg2 = glm(ACT ~ OWSF + OWSS + WWSS + SPEC + TRAIN + , family = binomial(link = 'logit'), data = sale_data)
 
 
-		LEAD	TEXP	TSERV	JOIN	LEAV	SIZE	OPEN	CEXP	CSERV	FAM	ASTAT	DUR	ITEMS	LTIME	PAY
+
