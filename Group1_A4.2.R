7db109fc95de28f33ea8fd715e8c264aa84b226a
Suzukaze <destroyersuzukaze@outlook.com>
2023-07-12 10:57:40 +0200

    .

---
+++ Group1_A4.2.R
@@ -0,0 +1,100 @@
+library(plyr)
+library(stargazer)
+library(sandwich) 
+library(xts)
+library(pander)
+library(lmtest)
+library(formattable)
+library(data.table)
+library(mvtnorm)
+library(ggplot2)
+library(MASS)
+library(tidyverse)
+library(readxl)
+library(car)
+library(psych)
+library(lubridate)
+library(stringr)
+library(rmarkdown)
+library(knitr)
+library(DT)
+library(magrittr)
+library(png)
+library(plm)
+
+options(digits=3)
+
+panelds = read_excel("./Tutorial/3_PanelTutorial_Data_Indulgence.xlsx",col_names = TRUE)
+
+panelds = panelds[,2:16]
+
+price_by_redp = panelds %>% group_by(redemption) %>% summarize(avg_price = sum(avg_item_price_dollar)/n())
+
+ln_panelds = log(panelds+1)
+colnames(ln_panelds) = paste("ln", colnames(ln_panelds), sep = '_')
+ln_panelds = cbind(panelds,ln_panelds)
+
+
+poollm = lm(avg_item_price_dollar~
+              redemption
+            + n_items_guest
+            + post_redemption
+            + points_rule_15
+            + n_guests_final
+            + time_since_last
+            + membership_length
+            + weekend
+            + lunch
+            + birthday
+            + visit_id
+            + as.factor(Brand_id)
+            , data = panelds)
+
+summary(poollm)
+
+#bptest(poollm)
+
+
+fixlm = plm(ln_avg_item_price_dollar~
+              redemption
+            + n_items_guest
+            + post_redemption
+            + points_rule_15
+            + n_guests_final
+            + time_since_last
+            + membership_length
+            + weekend
+            + lunch
+            + birthday
+            + as.numeric(visit_id)
+            + as.factor(Brand_id)
+            , data = ln_panelds, model = "within", index = c("CustomerID", "visit_id") )
+
+summary(fixlm)
+
+randlm = plm(ln_avg_item_price_dollar~
+              redemption
+            + n_items_guest
+            + post_redemption
+            + points_rule_15
+            + n_guests_final
+            + time_since_last
+            + membership_length
+            + weekend
+            + lunch
+            + birthday
+            + as.numeric(visit_id)
+            + as.factor(Brand_id)
+            , data = ln_panelds, model = "random", index = c("CustomerID", "visit_id") )
+
+summary(randlm)
+
+
+
+
+
+
+
+
+
+
